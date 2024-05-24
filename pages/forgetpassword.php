a09de737dda928bc993deba9c781b8d77e9c3f73
Pizon <thanhhangdang16@gmail.com>
2024-05-25 06:20:25 +0700

    fix

--- pages/forgetpassword.php
+++ pages/forgetpassword.php
@@ -39,7 +39,7 @@
                         $mail->isSMTP();
                         $mail->Host = 'smtp.gmail.com';
                         $mail->SMTPAuth = true;
-                        $mail->Username = 'thanhhangdang16@gmail.com';
+                        $mail->Username = '';
                         $mail->Password = 'mfeu muld zejs ualx';
                         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
                         $mail->Port = 587;
